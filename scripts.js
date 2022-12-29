e58cc36d43601e00a8d820a0b9f1486054056d3c
Tuomas Partanen <tuomashp@gmail.com>
2022-12-29 12:46:31 +0200

    Villains and veterans

--- scripts.js
+++ scripts.js
@@ -1,22 +1,62 @@
+function getRandomGreeting(maxWidth) {
+    var resp = '';
+
+    return resp;
+}
+
+function readFromTree(directory, file) {
+    console.log('readFromTree(' + directory + ', ' + file + ')');
+
+    fetch('https://tupartan.github.io/tree.json')
+        .then(response => {
+            console.log("Found: " + response);
+        })
+        .catch(error => {
+            console.error("Not found: " + error);
+        });
+
+    var d = directory;
+    var f = file;
+
+    var resp = '';
+
+    // If directory is not set, then use root as default
+    if(typeof d === 'undefined' || d === null) {
+        d = '.';
+    }
+    
+    if (typeof f === 'undefined' || f === null) {
+        // If file is not defined, return contents of directory
+
+    } else {
+        // Return file contents
+    }
+
+    return resp;
+
+}
+
 function printHelp() {
-    var text = "List of available commands\n bar";
+    
+    var text = 'List of available commands\n bar';
     return text;
 
 }
 
 function toggleGui() {
 
-    var resp = "";
-    var x = document.getElementById("gui");
+    var resp = '';
+    var x = document.getElementById('gui');
 
-    console.log("Style: " + x.style.display)
+    console.log('Style: ' + x.style.display)
 
-    if (x.style.display === "none" || x.style.display === "") {
+    if (x.style.display === 'none' || x.style.display === '') {
         resp = 'Starting graphical user interface';
-        x.style.display = "block";
+        x.style.display = 'block';
     } else {
         resp = 'Exiting graphical user interface';
-        x.style.display = "none";
+        $('body').terminal().echo(resp);
+        x.style.display = 'none';
     }
     return resp;
 
